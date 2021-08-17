49ba33265741ad9a75c7714559c1f6cfde611d77
kevinfabianp <kevin.fabian@rjberru.com>
2021-08-17 13:57:23 -0500

    cambios ultimos

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { FooterComponent } from '../layout/footer/footer.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 

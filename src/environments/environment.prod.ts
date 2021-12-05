0defc32f0137088681346ef71661460beca7812a
kevinfabianp <kevin.fabian@rjberru.com>
2021-12-05 14:00:56 -0500

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   domain_url: 'https://despliegueapi.canvet.me', 
-  //captcha : '6Lc1mlwdAAAAAG90Hl9PIaez1hrbN-0wQ41XSmvD'
-  captcha : '6LfsrAkcAAAAAFQruh-XZdBhRXAOHGJwvs8EQpb4' //localhost
+  captcha : '6Lc1mlwdAAAAAG90Hl9PIaez1hrbN-0wQ41XSmvD' //deploy
+  //captcha : '6LfsrAkcAAAAAFQruh-XZdBhRXAOHGJwvs8EQpb4' //localhost
 };
